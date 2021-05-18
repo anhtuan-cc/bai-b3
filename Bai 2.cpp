71eeb54ac889fed6d926379151c85cfc815b3d6d
anhtuan-cc <nguyenanhtuan.tc1991@gmail.com>
2021-05-18 17:35:31 +0700

    nop bai

---
+++ Bai 2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+  int main(){
+  	int a,b;
+  	printf("so tien gui ban dau la: \n");
+  	scanf("%d",&a);
+  	printf("so nam gui: \n");
+  	scanf("%d",&b);
+  	double c;
+  	c=a*pow((108/100),b);
+  	printf("tong tien %lf",c);
+ 
+  }
+
